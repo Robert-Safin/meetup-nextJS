30c0af74356742bc749a0718b0968842e13b26e8
Robert Safin <robertsafin98@gmail.com>
2023-04-06 22:01:43 +0800

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
 import MeetUpList from '../components/meetups/MeetUpList'
 
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Netflix-avatar.png/600px-Netflix-avatar.png',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup'
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Netflix-avatar.png/600px-Netflix-avatar.png',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup'
+  }
+]
+
 const HomePage = () => {
   return (
-    <MeetUpList meetups={}></MeetUpList>
+    <MeetUpList meetups={DUMMY_MEETUPS}></MeetUpList>
   )
 }
 
